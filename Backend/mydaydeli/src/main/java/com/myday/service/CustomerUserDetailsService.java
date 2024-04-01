5435ec5c3a393beeb0f4c3b81093954c87f86fc1
Jangchaeyun <zzangsally@naver.com>
2024-04-01 22:47:50 +0900

    0401 CustomerUserDetailsService

--- Backend/mydaydeli/src/main/java/com/myday/service/CustomerUserDetailsService.java
+++ Backend/mydaydeli/src/main/java/com/myday/service/CustomerUserDetailsService.java
@@ -4,10 +4,15 @@ import com.myday.model.USER_ROLE;
 import com.myday.model.User;
 import com.myday.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CustomerUserDetailsService implements UserDetailsService {
@@ -23,8 +28,10 @@ public class CustomerUserDetailsService implements UserDetailsService {
 
         USER_ROLE role = user.getRole();
         if (role == null) role = USER_ROLE.ROLE_CUSTOMER;
+        List<GrantedAuthority> authorities = new ArrayList<>();
 
+        authorities.add(new SimpleGrantedAuthority(role.toString()));
 
-        return null;
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
     }
 }
