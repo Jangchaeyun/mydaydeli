cbcf0550c89a9432b4fd02b0f692d77fd99dc25e
Jangchaeyun <zzangsally@naver.com>
2024-04-23 22:06:49 +0900

    0423 AdminRestaurantController

--- Backend/mydaydeli/src/main/java/com/myday/controller/AdminRestaurantController.java
+++ Backend/mydaydeli/src/main/java/com/myday/controller/AdminRestaurantController.java
@@ -3,6 +3,7 @@ package com.myday.controller;
 import com.myday.model.Restaurant;
 import com.myday.model.User;
 import com.myday.request.CreateRestaurantRequest;
+import com.myday.response.MessageResponse;
 import com.myday.service.RestaurantService;
 import com.myday.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,42 @@ public class AdminRestaurantController {
 
         Restaurant restaurant = restaurantService.createRestaurant(req, user);
         return new ResponseEntity<>(restaurant, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Restaurant>  updateRestaurant(
+            @RequestBody CreateRestaurantRequest req,
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable Long id) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+
+        Restaurant restaurant = restaurantService.updateRestaurant(id, req);
+        return new ResponseEntity<>(restaurant, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MessageResponse>  deleteRestaurant(
+            @RequestBody CreateRestaurantRequest req,
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable Long id) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+
+       restaurantService.deleteRestaurant(id);
+
+        MessageResponse res = new MessageResponse();
+        res.setMessage("Restaurant deleted successfully");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/status")
+    public ResponseEntity<Restaurant> updateRestaurantStatus(
+            @RequestBody CreateRestaurantRequest req,
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable Long id) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+
+        Restaurant restaurant = restaurantService.updateRestaurantStatus(id);
+
+        return new ResponseEntity<>(restaurant, HttpStatus.OK);
     }
 }
