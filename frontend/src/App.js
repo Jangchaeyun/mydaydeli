cfa59ea4a2e78dea606a2a245b40abb9475437d9
Jangchaeyun <zzangsally@naver.com>
2024-05-02 22:05:17 +0900

    0502 Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
+import "./index.css";
 import { ThemeProvider } from "@emotion/react";
 import Navbar from "./component/NavBar/Navbar";
 import { darkTheme } from "./Theme/DarkTheme";
 import { CssBaseline } from "@mui/material";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Navbar />
+      <Home />
     </ThemeProvider>
   );
 }
--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="">
+      <section className="banner -z-50 relative flex flex-col justify-center items-center">
+        <div className="w-[50vw] z-10 text-center">
+          <p className="text-2xl lg:text-6xl font-bold z-10 py-5">mydaydeli</p>
+          <p className="z-10 text-gray-300 text-xl lg:text-4xl">
+            "한 입에 맛있는 혜택, 빠른 배달로 집으로 곧바로!"
+          </p>
+        </div>
+        <div className="cover absolute top-0 left-0 right-0"></div>
+        <div className=""></div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
