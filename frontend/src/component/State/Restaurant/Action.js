2a0c7eaae1d67eeb334c211353b994dc2aa713fe
Jangchaeyun <zzangsally@naver.com>
2024-05-10 23:43:54 +0900

    0510 Restaurant Action

--- frontend/src/component/State/Restaurant/Action.js
+++ frontend/src/component/State/Restaurant/Action.js
@@ -3,6 +3,12 @@ import {
   GET_ALL_RESTAURANTS_FAILURE,
   GET_ALL_RESTAURANTS_REQUEST,
   GET_ALL_RESTAURANTS_SUCCESS,
+  GET_RESTAURANT_BY_ID_FAILURE,
+  GET_RESTAURANT_BY_ID_REQUEST,
+  GET_RESTAURANT_BY_ID_SUCCESS,
+  GET_RESTAURANT_BY_USER_ID_FAILURE,
+  GET_RESTAURANT_BY_USER_ID_REQUEST,
+  GET_RESTAURANT_BY_USER_ID_SUCCESS,
 } from "./ActionTypes";
 
 export const getAllRestaurantsAction = (token) => {
@@ -19,6 +25,47 @@ export const getAllRestaurantsAction = (token) => {
     } catch (error) {
       console.log("catch error ", error);
       dispatch({ type: GET_ALL_RESTAURANTS_FAILURE, payload: error });
+    }
+  };
+};
+
+export const getRestaurantById = (reqData) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_RESTAURANT_BY_ID_REQUEST });
+    try {
+      const response = await api.get(
+        `api/restaurants/${reqData.restaurantId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${reqData.jwt}`,
+          },
+        }
+      );
+      dispatch({ type: GET_RESTAURANT_BY_ID_SUCCESS, payload: response.data });
+    } catch (error) {
+      console.log("error", error);
+      dispatch({ type: GET_RESTAURANT_BY_ID_FAILURE, payload: error });
+    }
+  };
+};
+
+export const getRestaurantByUserId = (jwt) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_RESTAURANT_BY_USER_ID_REQUEST });
+    try {
+      const { data } = await api.get(`api/admin/restaurant/user`, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      console.log("get restarurant by user id ", data);
+      dispatch({ type: GET_RESTAURANT_BY_USER_ID_SUCCESS, payload: data });
+    } catch (error) {
+      console.log("catch error ", error);
+      dispatch({
+        type: GET_RESTAURANT_BY_USER_ID_FAILURE,
+        payload: error.messge,
+      });
     }
   };
 };
